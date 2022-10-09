c6b77295785250ad35bedcdeb8677c3079dd09e1
Brian Rouse <brian.g.rouse@gmail.com>
2022-10-08 22:03:19 -0400

    Initial project

---
+++ README.md
@@ -0,0 +1,4 @@
+TODO: if you end up using webpack, consider using webpack dev server instead of reload
+
+TODO: make a script that will create local browser run static file
+    --> will need CSV to be available on GitHub
---
+++ public/parallax.js
@@ -0,0 +1,41 @@
+const getImageUrl = (data) => {
+    if (!data.Image) {
+        return null
+    }
+    return `url('/images/${data.Image}')`
+}
+
+const generateParallax = () => {
+    if (typeof d3 === 'undefined') {
+        console.error("D3 not loaded")
+        return
+    }
+
+    d3.csv("./data/parks_data.csv").then(data => {
+        const parkInfo = d3
+            .select("#parks-visualization")
+            .selectAll("div")
+            .data(data)
+            .join("div")
+            .attr("class", "park-image-container")
+            .style("background-image", d => getImageUrl(d))
+            .append("div")
+            .attr("class", "park-info content")
+        
+        parkInfo
+            .append("h2")
+            .text(d => `${d.Act} (${d.Year})`)
+        
+        parkInfo
+            .append("p")
+            .text(d => d["Description Text"])
+
+        d3.select(".loading-background")
+            .transition()
+            .duration(2000)
+            .style("opacity", 0);
+    })
+    
+}
+
+generateParallax()
